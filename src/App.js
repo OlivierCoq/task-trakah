318caacfba9c8abd782cc97b62a67dab8bf510fb
OlivierC <coq.olivier36@gmail.com>
2022-12-29 18:25:00 -0500

    updating functions with api

--- src/App.js
+++ src/App.js
@@ -24,26 +24,58 @@ useEffect(() => {
 const fetchTasks = async () => {
   const res = await fetch('http://localhost:5002/tasks'),
         data = await res.json()
+        // console.log('data', data)
+        return data
+}
 
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5002/tasks/${id}`),
+        data = await res.json()
         // console.log('data', data)
         return data
 }
 
     // Delete
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+      // API:
+    await fetch(`http://localhost:5002/tasks/${id}`, { method: 'DELETE'})
+      // UI
     setTasks(tasks.filter((task) => task.id !== id))
   }
     // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
     // console.log('toggling', id)
+    const target_task = await fetchTask(id),
+          updateTask = {...target_task, reminder: !target_task.reminder}
+    
+    const res = await fetch(`http://localhost:5002/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+          'Content-type': 'application/json'
+      },
+        body: JSON.stringify(updateTask)
+      })
+      const data = await res.json()
+       
+      // UI
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
   }
 
     // AddNew
-  const addTask = (task) => {
+  const addTask = async (task) => {
     // console.log('adding', task)
-    const id = Math.floor(Math.random() * 1000) + 1, newTask = {...task, id}
-    setTasks([...tasks, newTask])
+    const res = await fetch('http://localhost:5002/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
+      // UI
+    // const id = Math.floor(Math.random() * 1000) + 1, newTask = {...task, id}
+    // setTasks([...tasks, newTask])
   }
 
 // TEMPLATE: 
